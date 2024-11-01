e702e0cb109cc38ca7a0faedbf13f57566f29d89
David Bresteau <david.bresteau@cea.fr>
2024-11-01 15:36:20 +0100

    Try to initialize the Arduino board.

--- src/pymodaq_plugins_arduino_ubuntu/daq_viewer_plugins/plugins_0D/daq_0Dviewer_ArduinoUbuntu.py
+++ src/pymodaq_plugins_arduino_ubuntu/daq_viewer_plugins/plugins_0D/daq_0Dviewer_ArduinoUbuntu.py
@@ -9,45 +9,35 @@ from pymodaq.utils.parameter import Parameter
 from pyfirmata2 import Arduino
 
 
-# TODO:
-# (1) change the name of the following class to DAQ_0DViewer_TheNameOfYourChoice
-# (2) change the name of this file to daq_0Dviewer_TheNameOfYourChoice ("TheNameOfYourChoice" should be the SAME
-#     for the class name and the file name.)
-# (3) this file should then be put into the right folder, namely IN THE FOLDER OF THE PLUGIN YOU ARE DEVELOPING:
-#     pymodaq_plugins_my_plugin/daq_viewer_plugins/plugins_0D
 class DAQ_0DViewer_ArduinoUbuntu(DAQ_Viewer_base):
     """ Instrument plugin class for a OD viewer.
     
     This object inherits all functionalities to communicate with PyMoDAQ’s DAQ_Viewer module through inheritance via
     DAQ_Viewer_base. It makes a bridge between the DAQ_Viewer module and the Python wrapper of a particular instrument.
 
-    TODO Complete the docstring of your plugin with:
-        * The set of instruments that should be compatible with this instrument plugin.
-        * With which instrument it has actually been tested.
-        * The version of PyMoDAQ during the test.
-        * The version of the operating system.
-        * Installation instructions: what manufacturer’s drivers should be installed to make it run?
+    This plugin is made to read an analog output of an Arduino board with PyMoDAQ.
+    Tested with an Arduino Uno, Arduino IDE 2.2.3
+    Firmata Standard server
+    Pyfirmata2 2.5.0
+    Pymodaq 4.4.6
+    Xubuntu 22.04
+
+    The Firmata server should be uploaded into the connected Arduino board with the Arduino IDE.
+    Instructions can be found here: https://pypi.org/project/pyFirmata2/#description or here
+    https://github.com/berndporr/pyFirmata2
 
     Attributes:
     -----------
     controller: object
         The particular object that allow the communication with the hardware, in general a python wrapper around the
          hardware library.
-         
-    # TODO add your particular attributes here if any
-
     """
     params = comon_parameters+[
-        ## TODO for your custom plugin: elements to be added here as dicts in order to control your custom stage
         ]
 
     def ini_attributes(self):
-        #  TODO declare the type of the wrapper (and assign it to self.controller) you're going to use for easy
-        #  autocompletion
-        self.controller: PythonWrapperOfYourInstrument = None
 
-        #TODO declare here attributes you want/need to init with a default value
-        pass
+        self.controller: Arduino = None
 
     def commit_settings(self, param: Parameter):
         """Apply the consequences of a change of value in the detector settings
@@ -57,11 +47,6 @@ class DAQ_0DViewer_ArduinoUbuntu(DAQ_Viewer_base):
         param: Parameter
             A given parameter (within detector_settings) whose value has been changed by the user
         """
-        ## TODO for your custom plugin
-        if param.name() == "a_parameter_you've_added_in_self.params":
-           self.controller.your_method_to_apply_this_param_change()  # when writing your own plugin replace this line
-#        elif ...
-        ##
 
     def ini_detector(self, controller=None):
         """Detector communication initialization
@@ -79,22 +64,26 @@ class DAQ_0DViewer_ArduinoUbuntu(DAQ_Viewer_base):
             False if initialization failed otherwise True
         """
 
-        raise NotImplemented  # TODO when writing your own plugin remove this line and modify the one below
         self.ini_detector_init(slave_controller=controller)
 
         if self.is_master:
-            self.controller = PythonWrapperOfYourInstrument()  #instantiate you driver with whatever arguments are needed
-            self.controller.open_communication() # call eventual methods
+            self.controller = Arduino
+
+            PORT = Arduino.AUTODETECT
+            ## If AUTODETECT fails, enter the port name manually
+            ## PORT = /ttyACM0
 
         # TODO for your custom plugin (optional) initialize viewers panel with the future type of data
-        self.dte_signal_temp.emit(DataToExport(name='myplugin',
+        self.dte_signal_temp.emit(DataToExport(name='arduino_ubuntu',
                                                data=[DataFromPlugins(name='Mock1',
                                                                     data=[np.array([0]), np.array([0])],
                                                                     dim='Data0D',
                                                                     labels=['Mock1', 'label2'])]))
 
-        info = "Whatever info you want to log"
-        initialized = self.controller.a_method_or_atttribute_to_check_if_init()  # TODO
+        info = "Board initialized!"
+
+        if PORT:
+            initialized = True
         return info, initialized
 
     def close(self):
